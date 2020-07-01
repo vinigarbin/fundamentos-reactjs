144b5db4a7f30b14ce9ed7cc8ec590be69c86f5b
Vinicius Garbin <vinigarbin@gmail.com>
2020-06-30 23:16:22 -0300

    ajustando requisicoes nas rotas

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,14 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+const formatValue = (value: number, type = 'currency'): string => {
+  if (type === 'date') {
+    return Intl.DateTimeFormat('pt-BR').format(value);
+  }
 
+  return Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value); // TODO
+};
+
+/*
+ */
 export default formatValue;
